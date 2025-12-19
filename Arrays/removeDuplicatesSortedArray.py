6fbe24ca58aee858c173e08a2c59d073a2040591
mallorysmith64 <mallorysmith64@gmail.com>
2025-12-19 11:31:36 -0500

    remove duplicates from sorted array solution added

---
+++ Arrays/removeDuplicatesSortedArray.py
@@ -0,0 +1,42 @@
+# Given an integer array nums sorted in non-decreasing order, 
+# remove the duplicates in-place such that each unique element appears only once. 
+# The relative order of the elements should be kept the same.
+
+# Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. 
+# After removing duplicates, return the number of unique elements k.
+
+# The first k elements of nums should contain the unique numbers in sorted order. 
+# The remaining elements beyond index k - 1 can be ignored.
+
+# Custom Judge:
+
+# The judge will test your solution with the following code:
+
+# int[] nums = [...]; // Input array
+# int[] expectedNums = [...]; // The expected answer with correct length
+
+# int k = removeDuplicates(nums); // Calls your implementation
+
+# assert k == expectedNums.length;
+# for (int i = 0; i < k; i++) {
+#     assert nums[i] == expectedNums[i];
+# }
+# If all assertions pass, then your solution will be accepted.
+
+# Example 1:
+
+# Input: nums = [1,1,2]
+# Output: 2, nums = [1,2,_]
+# Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+        j = 1
+
+        for i in range(1, n):
+            if nums[i] != nums[i-1]:
+                nums[j] = nums[i]
+                j += 1
+        return j
