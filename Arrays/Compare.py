8dd753029783e2baecdf6224782feb831f7f57eb
mallorysmith64 <mallorysmith64@gmail.com>
2025-12-23 11:49:58 -0500

    added maximal square

---
+++ Arrays/Compare.py
@@ -0,0 +1,39 @@
+# # Intuition
+# <!-- Describe your first thoughts on how to solve this problem. -->
+
+# # Approach
+# <!-- Describe your approach to solving the problem. -->
+
+# # Complexity
+# - Time complexity:
+# <!-- Add your time complexity here, e.g. $$O(n)$$ -->
+
+# - Space complexity:
+# <!-- Add your space complexity here, e.g. $$O(n)$$ -->
+
+# # Code
+# ```python3 []
+from typing import List
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+
+        rows, cols = len(matrix), len(matrix[0])
+         # dp[j] will store the side length of the square ending at column j
+        dp = [0] * (cols + 1)
+        max_side = 0
+        prev = 0
+
+        for i in range(1, rows + 1):
+            for j in range(1, cols + 1):
+                temp = dp[j]
+                if matrix[i-1][j-1] == "1":
+                    dp[j] = min(dp[j], dp[j-1], prev) + 1
+                    max_side = max(max_side, dp[j])
+                else:
+                    dp[j] = 0
+                prev = temp
+
+        return max_side * max_side
